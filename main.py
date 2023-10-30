7b6a1fc710ee099bba7a473944f75b30c946059e
itzrossyo <rossespley@gmail.com>
2023-10-30 22:54:02 +0000

    basic layout has been done with bootstrap forms and a few routes

--- main.py
+++ main.py
@@ -1,12 +1,47 @@
-from flask import Flask, jsonify
+from flask import Flask, render_template, url_for
+from forms import RegistrationForm, LoginForm
 import os
 
 app = Flask(__name__)
+app.config['SECRET_KEY'] = 'cV.HIE?4g90ag~?8*YM:NuTHMGGw%~'
+
+posts = [
+    {
+        'author': ' Corey schafer',
+        'title': 'Blog Post 1',
+        'content': 'first post',
+        'date_posted': 'April 20, 2018'
+    },
+    {
+        'author': ' Corey schafer',
+        'title': 'Blog Post 2',
+        'content': 'second post',
+        'date_posted': 'April 22, 2018'
+    }
+]
 
 
 @app.route('/')
-def index():
-    return jsonify({"Choo Choo": "Welcome to your Flask app 🚅"})
+@app.route('/home')
+def home():
+    return render_template('home.html', posts=posts)
+
+
+@app.route("/about")
+def about():
+    return render_template('about.html', title='About')
+
+
+@app.route("/register")
+def register():
+    form = RegistrationForm()
+    return render_template('register.html', title='Register', form=form)
+
+
+@app.route("/login")
+def login():
+    form = LoginForm()
+    return render_template('login.html', title='Login', form=form)
 
 
 if __name__ == '__main__':
