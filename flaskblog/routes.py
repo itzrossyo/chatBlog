745ec36a4090a047d3331d764667b7d920f6887d
itzrossyo <rossespley@gmail.com>
2023-10-31 02:33:53 +0000

    added so that users will be redirected to the login page if trying to directly to accounts and will be aske to log in

--- flaskblog/routes.py
+++ flaskblog/routes.py
@@ -2,7 +2,7 @@ from flask import render_template, url_for, flash, redirect, request
 from flaskblog import app, db, bcrypt
 from flaskblog.forms import RegistrationForm, LoginForm
 from flaskblog.models import User, Post
-from flask_login import login_user, current_user, logout_user
+from flask_login import login_user, current_user, logout_user, login_required
 
 posts = [
     {
@@ -62,7 +62,13 @@ def login():
     return render_template('login.html', title='Login', form=form)
 
 
-@app.route("/logout",)
+@app.route("/logout", )
 def logout():
     logout_user()
     return redirect(url_for('home'))
+
+
+@app.route("/account")
+@login_required
+def account():
+    return render_template('account.html', title='account')
