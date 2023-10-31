4cc134b54977a37c273f150ac17ff9638dbc1b4b
itzrossyo <rossespley@gmail.com>
2023-10-31 01:36:02 +0000

    added password hashing for users and succesfully creates and account to the db

--- run.py
+++ run.py
@@ -1,4 +1,6 @@
-from flaskblog import app
+from flaskblog import app, db
 import os
 if __name__ == '__main__':
+    with app.app_context():
+        db.create_all()
     app.run(debug=True, port=os.getenv("PORT", default=5000))
