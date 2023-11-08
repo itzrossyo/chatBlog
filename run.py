4b3f400926067e38bab729110d1b0ab3d01ea934
itzrossyo <rossespley@gmail.com>
2023-11-08 05:54:49 +0000

    fixed all structre for files and move secret key and all sensative things to evn variables

--- run.py
+++ run.py
@@ -1,7 +1,6 @@
-from flaskblog import app, db
-import os
+from flaskblog import create_app
+
+app = create_app()
 
 if __name__ == '__main__':
-    with app.app_context():
-        db.create_all()
-    app.run(debug=True, port=os.getenv("PORT", default=5000))
+    app.run(debug=True)
