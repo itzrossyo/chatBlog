847255410c541a5ebfbd49539aa4d9f68e8b17a1
itzrossyo <rossespley@gmail.com>
2023-11-08 06:13:20 +0000

    added errors pages for 500,404,403

--- flaskblog/__init__.py
+++ flaskblog/__init__.py
@@ -12,10 +12,6 @@ login_manager.login_view = 'users.login'
 login_manager.login_message_category = 'info'
 mail = Mail()
 
-from flaskblog.users.routes import users
-from flaskblog.posts.routes import posts
-from flaskblog.main.routes import main
-
 
 def create_app(config_class=Config):
     app = Flask(__name__)
@@ -26,8 +22,13 @@ def create_app(config_class=Config):
     login_manager.init_app(app)
     mail.init_app(app)
 
+    from flaskblog.users.routes import users
+    from flaskblog.posts.routes import posts
+    from flaskblog.main.routes import main
+    from flaskblog.errors.handlers import errors
     app.register_blueprint(users)
     app.register_blueprint(posts)
     app.register_blueprint(main)
+    app.register_blueprint(errors)
 
     return app
